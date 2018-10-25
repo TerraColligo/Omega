bd64ef1067841249a22f37fcf03bca4177807540
Ossi Mantylahti <37711285+ossimantylahti@users.noreply.github.com>
2018-10-25 12:13:05 -0500

    commission v2

--- tc_sales_commission/models/sale.py
+++ tc_sales_commission/models/sale.py
@@ -7,7 +7,6 @@ class res_partner(models.Model):
 
     sale_repr_id = fields.Many2one('res.partner', string="Sales Representative")
     comm_per = fields.Float(string="Percentage")
-    sale_commission_apply = fields.Selection([('invoiced','Invoiced'),('paid','Paid')])
 
     @api.one
     @api.constrains('comm_per')
@@ -22,33 +21,14 @@ class sale_order(models.Model):
     @api.multi
     @api.depends('amount_untaxed', 'amount_total', 'sale_repr_id', 'comm_per')
     def _get_total_commission_amount(self):
-        amount = 0.0
-        total_amount = 0.0
-        comm_amount = 0.0
         for order in self:
-            if order.invoice_ids:
-                invoice_ids = order.invoice_ids 
-#                 if order.sale_commission_apply == 'invoiced':
-#                     invoice_ids = order.invoice_ids.filtered(lambda ai:ai.state in ('open','paid'))
-#                 elif order.sale_commission_apply == 'paid':
-#                     invoice_ids = order.invoice_ids.filtered(lambda ai:ai.state=='paid')
-#                 else:
-#                     invoice_ids = order.invoice_ids
-                for inv in invoice_ids:
-                    amount += inv.amount_untaxed
-#                if order.amount_total == sum(invoice_ids.mapped('amount_total')): 
-#                     cancel_invoice = order.invoice_ids.filtered(lambda ai:ai.state=='cancel')
-#                     for inv in cancel_invoice:
-#                         cancel_inv_amount += inv.amount_untaxed
-                total_amount = amount
-                comm_amount = (total_amount * order.comm_per) / 100
+            comm_amount = (order.amount_untaxed * order.comm_per) / 100
             order.commission_amount = comm_amount
 
     sale_repr_id = fields.Many2one('res.partner', string="Agent")
     comm_per = fields.Float(string="Percentage",default=0.0)
     commission_amount = fields.Monetary(string="Commission", compute="_get_total_commission_amount")
-    sale_commission_apply = fields.Selection([('invoiced','Invoiced'),('paid','Paid')])
-    
+
     @api.one
     @api.constrains('comm_per')
     def _check_comm_per_value(self):
@@ -63,7 +43,6 @@ class sale_order(models.Model):
         if self.partner_id:
             self.sale_repr_id = self.partner_id.sale_repr_id
             self.comm_per = self.partner_id.comm_per or 0.0
-            #self.sale_commission_apply = self.partner_id.sale_commission_apply
 
     @api.onchange('sale_repr_id')
     def onchange_agent(self):
