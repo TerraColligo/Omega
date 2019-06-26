0ba06fbbd681f1fa2be4625f3b5133e42b21e155
Ossi Mantylahti <37711285+ossimantylahti@users.noreply.github.com>
2019-06-26 06:18:57 +0100

    contact data export

---
+++ tc_contacts_data_export/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Contact Export',
+    'version': '11.0.25.06.2019',
+    'category': 'base',
+    'depends': ['contacts', 'account', 'sun_sales_commission'],
+    'author': "",
+    'license': 'OPL-1',
+    'website': '',
+    'data': [
+        'wizard/export_partner_view.xml',
+        'views/res_partner_views.xml'
+    ],
+    "qweb": [],
+    'installable': True,
+    'application': True,
+ }
